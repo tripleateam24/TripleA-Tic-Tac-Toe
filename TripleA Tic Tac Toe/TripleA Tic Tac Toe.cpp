0e67289b366f3848403b54608818edc166c79255
Alphonso Renzulli <ajrenzulli@outlook.com>
2023-09-18 13:17:22 -0400

    display ui

--- TripleA Tic Tac Toe/TripleA Tic Tac Toe.cpp
+++ TripleA Tic Tac Toe/TripleA Tic Tac Toe.cpp
@@ -25,14 +25,74 @@ void display_ui() {
     cout << "     |     |    \n";
 }
 
+//Function to get the player input and update the board
+
+void player_turn()
+{
+    if (player_turnchar == 'X')
+        cout << "Player 1 [X] turn : ";
+    else if (player_turnchar == 'O')
+        cout << "Player 2 [O] turn : ";
+    cin >> choice; //geting the placement input from user
+    switch (choice) { //switch case to get which row and column will be update (9 places for a move 9 cases)
+    case 1: row = 0; column = 0; break;
+    case 2: row = 0; column = 1; break;
+    case 3: row = 0; column = 2; break;
+    case 4: row = 1; column = 0; break;
+    case 5: row = 1; column = 1; break;
+    case 6: row = 1; column = 2; break;
+    case 7: row = 2; column = 0; break;
+    case 8: row = 2; column = 1; break;
+    case 9: row = 2; column = 2; break;
+    default:
+        cout << "Invalid Move";
+    }
+    if (player_turnchar == 'X' && board[row][column] != 'X' && board[row][column] != 'O') {
+        //making sure to check that there is an open space and it is the correct players turn
+        board[row][column] = 'X'; // putting the correct player's move into the spot selected on the board
+        player_turnchar = 'O'; //switching the move to the other player
+    }
+    else if (player_turnchar == 'O' && board[row][column] != 'X' && board[row][column] != 'O') {
+        board[row][column] = 'O'; //repeating the process from above
+        player_turnchar = 'X';
+    }
+    else {
+        cout << "Box already filled!n Please choose another!!nn";
+        player_turn();//iand f input position already filled throws error message and restarts turn
+    }
+    display_ui(); //displaying board at end of turn for next turn
+
+}
+
+bool gameover() {
+
+    for (int i = 0; i < 3; i++)
+        if (board[i][0] == board[i][1] && board[i][0] == board[i][2] || board[0][i] == board[1][i] && board[0][i] == board[2][i])
+            return false; //this is a very simple way of checking for a win through col and row
+
+    if (board[0][0] == board[1][1] && board[0][0] == board[2][2] || board[0][2] == board[1][1] && board[0][2] == board[2][0])
+        return false; //this checks for a win in both diagonals
+
+    for (int i = 0; i < 3; i++)
+        for (int j = 0; j < 3; j++)
+            if (board[i][j] != 'X' && board[i][j] != 'O')
+                return true;//this checks to see if the game is still being played or has stopped by win 
+
+    draw = true;//Checking to see if game already draw
+    return false;
+}
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << "TIC TAC TOE GAME FOR 2 PLAYERS\n";
+
+
+    //just need a while or for loop for the objects to run through, then some if statements to check if a win/draw happened then a text prompt for a win or draw
 }
 
 
-    // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-    // Debug program: F5 or Debug > Start Debugging menu
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
 
 // Tips for Getting Started: 
 //   1. Use the Solution Explorer window to add/manage files
